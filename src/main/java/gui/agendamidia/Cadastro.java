78bb33d02b6edb197938e42aa1eb8a7c81016058
debedantas <dantasdeborah5@gmail.com>
2024-01-28 17:33:22 -0300

    Add cadastro logic

--- src/main/java/gui/agendamidia/Cadastro.java
+++ src/main/java/gui/agendamidia/Cadastro.java
@@ -1,20 +1,27 @@
 package gui.agendamidia;
 
+import BD.UsuarioController;
+import exceptions.UsuarioJaExisteException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import modelo.TipoUsuario;
+import modelo.Usuario;
+
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Cadastro implements Initializable {
-    private ObservableList<String> contaSelectorItems = FXCollections.observableArrayList("Padrão", "Administrador", "Crítico");
+    private ObservableList<String> contaSelectorItems = FXCollections.observableArrayList("Padrao", "Admin", "Critico");
 
     @FXML
     private AnchorPane cena;
@@ -24,9 +31,6 @@ public class Cadastro implements Initializable {
 
     @FXML
     private TextField login;
-
-    @FXML
-    private TextField nome;
 
     @FXML
     private PasswordField senha;
@@ -43,12 +47,32 @@ public class Cadastro implements Initializable {
 
     @FXML
     void validar(ActionEvent event) {
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setTitle("Alerta");
+        alerta.setHeaderText(null);
 
+        if (login.getText().isEmpty() || senha.getText().isEmpty() || contaSelector.getValue().isEmpty()) {
+            alerta.setContentText("Todos os campos precisam ser preenchidos");
+            alerta.showAndWait();
+        } else {
+            try {
+                UsuarioController uc = new UsuarioController();
+                Usuario user = new Usuario(login.getText(), senha.getText(), TipoUsuario.valueOf(contaSelector.getValue()));
+                uc.addUsuario(user); // sai do bloco caso já exista
+
+                alerta.setContentText("Usuario Cadastrado");
+                alerta.showAndWait();
+                showLogin(null);
+            } catch (SQLException | UsuarioJaExisteException e) {
+                alerta.setContentText("Usuario Já existe");
+                alerta.showAndWait();
+            }
+        }
     }
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         contaSelector.setItems(contaSelectorItems);
-        contaSelector.setValue("Padrão");
+        contaSelector.setValue("Padrao");
     }
 }
